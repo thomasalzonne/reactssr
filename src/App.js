a8dd02ef05340908e6629a8443fce527e4a8e517
thomasalzonne <thomas.alzonne@gmail.com>
2022-12-07 14:37:25 +0100

    ssr

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import React from 'react'
 
 function App() {
+  const [wines, setWines] = useState([]);
+
+  const getWines = () => {
+  return fetch('http://localhost:8000')
+    .then((response) => response.json())
+    .then((data) => data);
+  };
+
+  useEffect(() => {
+    getWines().then((wines) => setWines(wines));
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {wines && <div>Wines</div>}
+      {wines &&
+        wines.map( wine => 
+            <div>
+              {wine.name} - {wine.annee}
+            </div>
+          )
+      }
     </div>
   );
 }
